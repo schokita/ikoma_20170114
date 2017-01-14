5b8dce03a1046fd4d8d7abd9525a1b60d10d3e36
schokita <sho.okita.6@gmail.com>
2017-01-14 16:22:18 +0900

    Update App.js

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,7 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー//
+○kmlの読み込み
+//’’内は読み込むkmlのパス
+viewer.dataSources.add(Cesium.KmlDataSource.load('doc.kml'));
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー//
