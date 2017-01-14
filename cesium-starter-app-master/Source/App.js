750180caa305473378576d7ef0f99e7a1f166bca
schokita <sho.okita.6@gmail.com>
2017-01-14 16:44:52 +0900

    Update App.js

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://schokita.github.io/ikoma_20170114/生駒サンプル.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://github.com/schokita/ikoma_20170114/生駒サンプル.kml'));
